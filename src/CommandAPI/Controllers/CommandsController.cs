d15acaf9a80acadb5594173c7f0b6605c0e9e91b
Abdelrahman Fared <bedosport6@gmail.com>
2020-12-22 13:03:12 +0200

    Update 22/12/2020

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers
@@ -7,10 +8,27 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository){
+            _repository = repository;
+        }
+ 
+
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            return Ok(_repository.GetAllCommands());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
 }
